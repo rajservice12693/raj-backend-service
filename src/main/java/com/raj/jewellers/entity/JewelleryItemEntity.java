70764ec607969a1d60e7e9ab1790f7743dd15767
Pramod Sharma <pramod.sharma@e-zest.in>
2025-10-03 11:38:36 +0530

    Remove unused code

--- src/main/java/com/raj/jewellers/entity/JewelleryItemEntity.java
+++ src/main/java/com/raj/jewellers/entity/JewelleryItemEntity.java
@@ -15,9 +15,7 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "jewellery_items")
